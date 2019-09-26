bd1a38f54dce4233d0089cbc05a4aef0808eb928
Samuel Ameh <propersam2012@gmail.com>
2019-09-26 23:04:36 +0100

    update how the forecast methods work for ses

--- forecast/ets.go
+++ forecast/ets.go
@@ -8,10 +8,25 @@ import (
 	"github.com/rocketlaunchr/dataframe-go"
 )
 
+// SesModel is a struct that holds necessary
+// computed values for a forecasting result
+type SesModel struct {
+	testData       *dataframe.SeriesFloat64
+	fcastData      *dataframe.SeriesFloat64
+	initialLevel   float64
+	originValue    float64
+	smoothingLevel float64
+	alpha          float64
+	mae            float64
+	sse            float64
+	rmse           float64
+	mape           float64
+}
+
 // SimpleExponentialSmoothing performs forecasting based on the Exponential Smoothing algorithm.
 // It returns a SeriesFloat64 with the next m forecasted values in the series.
 // The argument α must be between [0,1]. Recent values receive more weight when α is closer to 1.
-func SimpleExponentialSmoothing(ctx context.Context, s *dataframe.SeriesFloat64, α float64, m int, r ...dataframe.Range) (*dataframe.SeriesFloat64, error) {
+func SimpleExponentialSmoothing(ctx context.Context, s *dataframe.SeriesFloat64, α float64, r ...dataframe.Range) (*SesModel, error) {
 
 	if len(r) == 0 {
 		r = append(r, dataframe.Range{})
@@ -32,16 +47,32 @@ func SimpleExponentialSmoothing(ctx context.Context, s *dataframe.SeriesFloat64,
 		return nil, errors.New("no values in series range")
 	}
 
-	if m <= 0 {
-		return nil, errors.New("m must be greater than 0")
-	}
-
 	if (α < 0.0) || (α > 1.0) {
 		return nil, errors.New("α must be between [0,1]")
 	}
 
-	forecast := make([]float64, 0, m)
+	trainedModel := &SesModel{
+		alpha:          α,
+		testData:       &dataframe.SeriesFloat64{},
+		fcastData:      &dataframe.SeriesFloat64{},
+		initialLevel:   0.0,
+		smoothingLevel: 0.0,
+		mae:            0.0,
+		sse:            0.0,
+		rmse:           0.0,
+		mape:           0.0,
+	}
+
+	testData := s.Values[end+1:]
+	testSeries := dataframe.NewSeriesFloat64("Test Data", nil)
+	testSeries.Values = testData
+
+	trainedModel.testData = testSeries
+
+	// tCount := len(testData)
+
 	var st float64
+
 	for i := start; i < end+1; i++ {
 		if err := ctx.Err(); err != nil {
 			return nil, err
@@ -51,10 +82,76 @@ func SimpleExponentialSmoothing(ctx context.Context, s *dataframe.SeriesFloat64,
 
 		if i == start {
 			st = xt
+			trainedModel.initialLevel = xt
 		} else {
 			st = α*xt + (1-α)*st
 		}
+
 	}
+
+	fcast := []float64{}
+	for k := end + 1; k < len(s.Values); k++ {
+		if err := ctx.Err(); err != nil {
+			return nil, err
+		}
+		xt := s.Values[k]
+
+		st = α*xt + (1-α)*st
+		fcast = append(fcast, st)
+
+		// Setting the last value in data as Yorigin value for bootstrapping
+		if k == len(s.Values)-1 {
+			trainedModel.originValue = s.Values[k]
+		}
+	}
+
+	fcastSeries := dataframe.NewSeriesFloat64("Forecast Data", nil)
+	fcastSeries.Values = fcast
+	trainedModel.fcastData = fcastSeries
+
+	trainedModel.smoothingLevel = st
+
+	opts := &ErrorOptions{}
+
+	mae, _, err := MeanAbsoluteError(ctx, testSeries, fcastSeries, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	sse, _, err := SumOfSquaredErrors(ctx, testSeries, fcastSeries, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	rmse, _, err := RootMeanSquaredError(ctx, testSeries, fcastSeries, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	mape, _, err := MeanAbsolutePercentageError(ctx, testSeries, fcastSeries, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	trainedModel.sse = sse
+	trainedModel.mae = mae
+	trainedModel.rmse = rmse
+	trainedModel.mape = mape
+
+	return trainedModel, nil
+}
+
+// Predict method is used to run future predictions for Ses
+// Using Bootstrapping method
+func (sm *SesModel) Predict(ctx context.Context, m int) (*dataframe.SeriesFloat64, error) {
+	if m <= 0 {
+		return nil, errors.New("m must be greater than 0")
+	}
+
+	forecast := make([]float64, 0, m)
+	α := sm.alpha
+	Yorigin := sm.originValue
+	st := sm.smoothingLevel
 
 	// Now calculate forecast
 	for range iter.N(m) {
@@ -62,11 +159,11 @@ func SimpleExponentialSmoothing(ctx context.Context, s *dataframe.SeriesFloat64,
 			return nil, err
 		}
 
-		st = α*s.Values[end] + (1-α)*st
+		st = α*Yorigin + (1-α)*st
 		forecast = append(forecast, st)
 	}
 
-	fdf := dataframe.NewSeriesFloat64("forecast", nil)
+	fdf := dataframe.NewSeriesFloat64("Prediction", nil)
 	fdf.Values = forecast
 
 	return fdf, nil
--- forecast/forecast_test.go
+++ forecast/forecast_test.go
@@ -5,61 +5,39 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/rocketlaunchr/dataframe-go"
 )
 
 func TestSes(t *testing.T) {
 	ctx := context.Background()
-	init := &dataframe.SeriesInit{}
 
 	// data := dataframe.NewSeriesFloat64("Complete Data", nil, 445.43, 345.2, 565.56, 433.34, 585.23, 593.32, 641.43, 654.35, 234.65, 567.45, 645.45, 445.34, 564.65, 598.76, 676.54, 654.56, 564.76, 456.76, 656.57, 765.45, 755.43, 745.2, 665.56, 633.34, 585.23, 693.32, 741.43, 654.35, 734.65, 667.45, 545.45, 645.34, 754.65, 798.76, 776.54, 654.56, 664.76, 856.76, 776.57, 825.45, 815.43, 845.2, 765.56, 733.34, 785.23, 893.32, 841.43, 754.35, 524.65, 567.45, 715.45, 845.34, 864.65, 898.76, 876.54, 854.56, 864.76, 856.76, 726.57, 700.31, 815.43, 805.2, 855.56, 733.34, 785.23, 893.32, 641.43, 554.35, 734.63, 834.89)
 	// m := 30
-	alpha := 0.5
+	alpha := 0.1
 
 	data := dataframe.NewSeriesFloat64("simple data", nil, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-	m := 5
+	m := 20
 
-	forecast, err := SimpleExponentialSmoothing(ctx, data, alpha, m, dataframe.Range{End: &[]int{8}[0]})
+	fModel, err := SimpleExponentialSmoothing(ctx, data, alpha, dataframe.Range{End: &[]int{5}[0]})
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
-
-	//testData := data.Values[0:40] // This is exact subset of data fetched with range passed into forecast
-	testData := data.Values[len(data.Values)-m:]
-	testSeries := dataframe.NewSeriesFloat64("Test SubData", init)
-
-	// Load forecast data into series
-	testSeries.Insert(testSeries.NRows(), testData[:])
+	spew.Dump(fModel)
 
 	fmt.Println(data.Table())
-	fmt.Println(testSeries.Table())
-	fmt.Println(forecast.Table())
+	fmt.Println(fModel.testData.Table())
+	fmt.Println(fModel.fcastData.Table())
 
-	opts := &ErrorOptions{}
-
-	mae, nvalMae, err := MeanAbsoluteError(ctx, testSeries, forecast, opts)
+	fpredict, err := fModel.Predict(ctx, m)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
 
-	sse, nvalSse, err := SumOfSquaredErrors(ctx, testSeries, forecast, opts)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-
-	rmse, nvalRmse, err := RootMeanSquaredError(ctx, testSeries, forecast, opts)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-
-	mape, nvalMape, err := MeanAbsolutePercentageError(ctx, testSeries, forecast, opts)
-	if err != nil {
-		t.Errorf("unexpected error: %s", err)
-	}
-
-	fmt.Println("alpha:", alpha)
-	fmt.Printf("MAE: %f, nvalues: %d\n\n", mae, nvalMae)
-	fmt.Printf("SSE: %f, nvalues: %d\n\n", sse, nvalSse)
-	fmt.Printf("RMSE: %f, nvalues: %d\n\n", rmse, nvalRmse)
-	fmt.Printf("MAPE: %f, nvalues: %d\n\n", mape, nvalMape)
+	fmt.Println(fpredict.Table())
+	fmt.Println("alpha:", fModel.alpha)
+	fmt.Printf("MAE: %f\n\n", fModel.mae)
+	fmt.Printf("SSE: %f\n\n", fModel.sse)
+	fmt.Printf("RMSE: %f\n\n", fModel.rmse)
+	fmt.Printf("MAPE: %f\n\n", fModel.mape)
 }
