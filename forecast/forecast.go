c4ee06ca4e1acf074e1278ecf35c121cecc0ed59
Samuel Ameh <propersam2012@gmail.com>
2019-10-04 16:26:10 +0100

    update model interface documentation

--- forecast/forecast.go
+++ forecast/forecast.go
@@ -9,10 +9,10 @@ import (
 // Model is an interface to group trained models of Different
 // Algorithms in the Forecast package under similar generic standard
 type Model interface {
-	// Fit Method performs the splitting and trainging of the SesModel based on the Exponential Smoothing algorithm.
-	// It returns a trained SesModel ready to carry out future forecasts.
+	// Fit Method performs the splitting and training of the Model Interface based on the Forecast algorithm Implemented.
+	// It returns a trained Model ready to carry out future forecasts.
 	// The argument α must be between [0,1]. Recent values receive more weight when α is closer to 1.
-	Fit(ctx context.Context, α float64, r ...dataframe.Range ) (*SesModel, error)
+	Fit(ctx context.Context, α float64, r ...dataframe.Range ) (Model, error)
 	// Predict method is used to run future predictions for Ses
 	// Using Bootstrapping method
 	Predict(ctx context.Context, m int) (*dataframe.SeriesFloat64, error)
